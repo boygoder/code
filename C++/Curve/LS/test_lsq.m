8a00a33fc3776000893ab76190b26ba070c3870a
boygoder <shr308177533@126.com>
2022-11-30 23:27:31 +0800

    to python

--- C++/Curve/LS/test_lsq.m
+++ C++/Curve/LS/test_lsq.m
@@ -2,10 +2,10 @@ clear;
 close all;
 
 %for f=  1 : 15 
-    file_path = '.\report\';
-    file_name = 'example2'%int2str(f);
+    file_path = '.\figure\';
+    file_name = '3';%int2str(f);
     file_ext = '.png';
     lsq(file_path,file_name,file_ext);
     clear; %清除上次运行的所有变量
-    close all; %关闭上次运行的子窗口
+    %close all; %关闭上次运行的子窗口
 %end
